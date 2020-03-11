072e324c23b34ad6bf865a26d0d85bb29a6c609e
Caio <ca.ragazzi@gmail.com>
2020-03-10 23:22:05 -0300

    first commit

---
+++ src/components/Dynamic.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="component" />
+</template>
+
+<script>
+import Input from "./Input";
+
+export default {
+  data() {
+    return {
+      component: Input
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>Oi</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
